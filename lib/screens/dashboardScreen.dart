883bcf7754651e6b457519572f5c75ba274d2083
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-08-18 19:08:11 -0400

    added dashboard skeleton

---
+++ lib/screens/dashboardScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatefulWidget {
+  @override
+  _DashboardScreenState createState() =>  _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text("Dashboard!"),
+      )
+    );
+  }
+}
