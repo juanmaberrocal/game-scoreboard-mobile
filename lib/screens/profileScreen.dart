a5b19b77ea5bed23cf070b59cc2795a6f8f91a44
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-08-28 09:45:35 -0400

    make profile consumer of curr user

--- lib/screens/profileScreen.dart
+++ lib/screens/profileScreen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 // dependencies
 import 'package:provider/provider.dart';
 // app
+import 'package:game_scoreboard/helpers/colorSelector.dart';
 import 'package:game_scoreboard/models/appProviders/currentPlayer.dart';
 import 'package:game_scoreboard/models/player.dart';
 import 'package:game_scoreboard/widgets/errorDisplay.dart';
@@ -16,21 +17,32 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  Player player;
-
-  @override
-  void initState() {
-    player = Provider.of<CurrentPlayer>(context, listen: false).player;
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(player.nickname),
-      ),
-      body: Text("foo"),
+    return Consumer<CurrentPlayer>(
+      builder: (context, CurrentPlayer, child) {
+        final Player player = CurrentPlayer.player;
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(player.nickname),
+          ),
+          body: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  minRadius: 50,
+                  backgroundColor: getColorFromString(player.nickname),
+                  child: Text("${player.firstName[0]}${player.lastName[0]}"),
+                ),
+                Text("${player.firstName} ${player.lastName}"),
+                Text(player.email),
+              ]
+            ),
+          ),
+        ); 
+      },
     );
   }
 }
