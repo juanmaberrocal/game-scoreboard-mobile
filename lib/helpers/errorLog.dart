70b83bc81e6935216a38c61424a6178d7131cbaf
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-08-29 10:45:27 -0400

    added rollbar notifications

---
+++ lib/helpers/errorLog.dart
@@ -0,0 +1,43 @@
+// flutter
+// dependencies
+import 'package:flutter_rollbar/flutter_rollbar.dart';
+// app
+
+Rollbar rollBar = Rollbar()
+    ..accessToken = '84e389f714904aca94e7201af6a09850'
+    ..environment = isInDebugMode ? 'development' : 'production';
+
+bool get isInDebugMode {
+  // Assume you're in production mode.
+  bool inDebugMode = false;
+
+  // Assert expressions are only evaluated during development. They are ignored
+  // in production. Therefore, this code only sets `inDebugMode` to true
+  // in a development environment.
+  assert(inDebugMode = true);
+
+  return inDebugMode;
+}
+
+Future<void> reportError(dynamic error, dynamic stackTrace) async {
+  // Print the exception to the console.
+  print('Caught error: $error');
+
+  if (isInDebugMode) {
+    // Print the full stacktrace in debug mode.
+    print(stackTrace);
+    return;
+  } else {
+    // Send the Exception and Stacktrace to Rollbar in Production mode.
+    rollBar.addTelemetry(
+        RollbarTelemetry(
+            level: RollbarLogLevel.ERROR,
+            type: RollbarTelemetryType.ERROR,
+            message: error.toString(),
+            stack: stackTrace.toString(),
+        ),
+    );
+
+    rollBar.publishReport(message: error.toString());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 // flutter
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 // dependencies
 // app
+import 'package:game_scoreboard/helpers/errorLog.dart';
 import 'package:game_scoreboard/models/appProviders/currentPlayer.dart';
 import 'package:game_scoreboard/models/appProviders/gamesLibrary.dart';
 import 'package:game_scoreboard/models/appProviders/playersLibrary.dart';
@@ -11,16 +13,40 @@ import 'package:game_scoreboard/screens/loginScreen.dart';
 import 'package:game_scoreboard/screens/dashboardScreen.dart';
 import 'package:game_scoreboard/screens/profileScreen.dart';
 
-void main() => runApp(
-  MultiProvider(
-    providers: [
-      ChangeNotifierProvider<CurrentPlayer>(builder: (context) => CurrentPlayer()),
-      ChangeNotifierProvider<GamesLibrary>(builder: (context) => GamesLibrary()),
-      ChangeNotifierProvider<PlayersLibrary>(builder: (context) => PlayersLibrary()),
-    ],
-    child: GameScoreboard(),
-  ),
-);
+Future<Null> main() async {
+  // This captures errors reported by the Flutter framework.
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    if (isInDebugMode) {
+      // In development mode simply print to console.
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      // In production mode report to the application zone to report to
+      // Rollbar.
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+
+  // This creates a [Zone] that contains the Flutter application and stablishes
+  // an error handler that captures errors and reports them.
+  //
+  // Using a zone makes sure that as many errors as possible are captured,
+  // including those thrown from [Timer]s, microtasks, I/O, and those forwarded
+  // from the `FlutterError` handler.
+  runZoned<Future<Null>>(() async {
+    runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<CurrentPlayer>(builder: (context) => CurrentPlayer()),
+          ChangeNotifierProvider<GamesLibrary>(builder: (context) => GamesLibrary()),
+          ChangeNotifierProvider<PlayersLibrary>(builder: (context) => PlayersLibrary()),
+        ],
+        child: GameScoreboard(),
+      ),
+    );
+  }, onError: (error, stackTrace) async {
+    await reportError(error, stackTrace);
+  });
+}
 
 class GameScoreboard extends StatelessWidget {
   // This widget is the root of your application.
