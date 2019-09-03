617e2e26143f962840c36226ca2c30b7c1145014
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-09-03 15:19:36 -0400

    fixed dart analysis errors

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:game_scoreboard_ui/main.dart';
+import 'package:game_scoreboard/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(GameScoreboard());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
