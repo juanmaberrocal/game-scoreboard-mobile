10c25093983fe201d17b5e02d5f19787f1b0c19f
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-09-09 17:41:35 -0400

    wip: init work to allow users to change avatar

--- lib/screens/profileScreen.dart
+++ lib/screens/profileScreen.dart
@@ -1,7 +1,9 @@
 // flutter
+import 'dart:async';
 import 'package:flutter/material.dart';
 // dependencies
 import 'package:provider/provider.dart';
+import 'package:image_picker/image_picker.dart';
 // app
 import 'package:game_scoreboard/models/appProviders/currentPlayer.dart';
 import 'package:game_scoreboard/models/player.dart';
@@ -16,17 +18,56 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  Future<void> _openAvatarOptions() {
+    return showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                GestureDetector(
+                  child: Text('Take a Picture'),
+                  onTap: () async {
+                    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+                  },
+                ),
+                Padding(padding: EdgeInsets.all(8.0)),
+                GestureDetector(
+                  child: Text('Select from Gallery'),
+                ),
+              ],
+            )
+          ),
+        );
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<CurrentPlayer>(
-      builder: (context, CurrentPlayer, child) {
-        final Player player = CurrentPlayer.player;
+      builder: (context, currentPlayer, child) {
+        final Player player = currentPlayer.player;
 
         return Scaffold(
           appBar: AppBar(
             title: Text(player.nickname),
           ),
           bottomNavigationBar: BottomNavigationBar(
+            onTap: (int index) {
+              switch(index) {
+                case 0: {
+                  _openAvatarOptions();
+                }
+                return;
+                case 1: {
+                  print("edit");
+                }
+                return;
+              }
+            },
             items: <BottomNavigationBarItem>[
               BottomNavigationBarItem(
                 icon: Icon(Icons.camera_alt),
@@ -37,7 +78,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 title: Text('Edit'),
               ),
             ],
-            selectedItemColor: Theme.of(context).textTheme.caption.color,
+            fixedColor: Theme.of(context).textTheme.caption.color,
           ),
           body: Padding(
             padding: EdgeInsets.all(20.0),
