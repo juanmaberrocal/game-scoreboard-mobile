53e445622a84bd36bb141f658997e473fca30b39
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-09-09 14:06:15 -0400

    imporved ui to display data in card and standings outside

--- lib/screens/gameScreen.dart
+++ lib/screens/gameScreen.dart
@@ -1,4 +1,5 @@
 // flutter
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // dependencies
 // app
@@ -32,51 +33,6 @@ class _GameScreenState extends State<GameScreen> {
     super.initState();
   }
 
-  Widget gameBody(Game game) {
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 200,
-          width: double.infinity,
-          margin: EdgeInsets.only(bottom: 30),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: false ? NetworkImage(
-                  "https://cf.geekdo-images.com/itemrep/img/aozRplCSOpRucLxSuClX2odEUBQ=/fit-in/246x300/pic2419375.jpg"
-              ) : AssetImage('assets/images/Missing-image-232x150.png'),
-              fit: BoxFit.contain,
-            ),
-          ),
-        ),
-        Text(game.description),
-        Container(
-          margin: EdgeInsets.all(30),
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                child: Column(
-                  children: <Widget>[
-                    Icon(Icons.group),
-                    Text("${game.minPlayers.toString()} - ${game.maxPlayers.toString()}"),
-                  ],
-                ),
-              ),
-              VerticalDivider(),
-              Expanded(
-                child: Column(
-                  children: <Widget>[
-                    Icon(Icons.timer),
-                    Text("${game.minPlayTime.toString()} - ${game.maxPlayTime.toString()}"),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,14 +40,14 @@ class _GameScreenState extends State<GameScreen> {
         title: Text(widget.gameName),
       ),
       body: Padding(
-        padding: EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(8.0),
         child: ListView(
           children: <Widget>[
             FutureBuilder<Game>(
                 future: _game,
                 builder: (BuildContext context, AsyncSnapshot<Game> snapshot) {
                   if (snapshot.hasData) {
-                    return gameBody(snapshot.data);
+                    return _GameBody(game: snapshot.data);
                   } else if (snapshot.hasError) {
                     return ErrorDisplay(
                       errorMessage: 'Could not load game data',
@@ -123,6 +79,64 @@ class _GameScreenState extends State<GameScreen> {
           ],
         ),
       )
+    );
+  }
+}
+
+class _GameBody extends StatelessWidget {
+  _GameBody({
+    Key key,
+    @required this.game
+  }) : super(key: key);
+
+  final Game game;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height / 3.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: false ? NetworkImage(
+                    "https://cf.geekdo-images.com/itemrep/img/aozRplCSOpRucLxSuClX2odEUBQ=/fit-in/246x300/pic2419375.jpg"
+                ) : AssetImage('assets/images/Missing-image-232x150.png'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(30),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    children: <Widget>[
+                      Icon(Icons.group),
+                      Text("${game.minPlayers.toString()} - ${game.maxPlayers.toString()}"),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: <Widget>[
+                      Icon(Icons.timer),
+                      Text("${game.minPlayTime.toString()} - ${game.maxPlayTime.toString()}"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 20.0),
+            child: Text(game.description),
+          ),
+        ],
+      ),
     );
   }
 }
