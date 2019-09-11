000fcda2be12d86b0554f9c4de6dd3427f7b2ca1
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-09-11 13:11:57 -0400

    added fallback color if empty string passed

--- lib/helpers/colorSelector.dart
+++ lib/helpers/colorSelector.dart
@@ -2,9 +2,15 @@
 import 'package:flutter/material.dart';
 
 Color getColorFromString(String string) {
-  final int colorIndex = string.toLowerCase().codeUnitAt(0) - 97;
-  final int colorOffset = colorIndex % 17;
-  final Color color = Colors.primaries[colorOffset];
+  Color color;
+
+  if (string.isEmpty) {
+    color = Colors.grey[200];
+  } else {
+    final int colorIndex = string.toLowerCase().codeUnitAt(0) - 97;
+    final int colorOffset = colorIndex % 17;
+    color = Colors.primaries[colorOffset];
+  }
 
   return color;
 }
