821655e5dd63bc571ef0d7dd236765741b0f7f97
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-09-19 08:48:45 -0400

    updated splash screen for responsive layout

--- lib/screens/splashScreen.dart
+++ lib/screens/splashScreen.dart
@@ -70,25 +70,75 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Center(
         child: Column(
           children: <Widget>[
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset("assets/images/logo.png"),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
-                    child: Text("Game Scoreboard", style: TextStyle(fontSize: 40.0, color: Colors.white),),
-                  ),
-                ]
-              )
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 30.0),
-              child: Text("Developed by Juanma", style: TextStyle(fontSize: 16.0, color: Colors.white),),
-            ),
+            Body(),
+            Footer(),
           ]
-        )
-      )
+        ),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  Body({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height / 3.0,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/logo.png'),
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            margin: EdgeInsets.only(top: 15.0),
+            child: FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text(
+                "Game Scoreboard",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 35.0, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Footer extends StatelessWidget {
+  Footer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10.0),
+      child: SizedBox(
+        height: 10,
+        width: double.infinity,
+        child: FittedBox(
+          fit: BoxFit.contain,
+          child: Text(
+            "Developed by Saludzinho™",
+            style: TextStyle(fontSize: 8.0, color: Colors.white),
+          ),
+        ),
+      ),
     );
   }
 }
