1c99fe6f80da9eb9d06e95783ba2b662ba19195f
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-09-20 15:39:58 -0400

    wip - reworking record screens to allow admin privaleges to edit

--- lib/screens/gameScreen.dart
+++ lib/screens/gameScreen.dart
@@ -1,9 +1,11 @@
 // flutter
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 // dependencies
 // app
+import 'package:game_scoreboard/models/appProviders/currentPlayer.dart';
 import 'package:game_scoreboard/models/game.dart';
+import 'package:game_scoreboard/widgets/appBlocks.dart';
 import 'package:game_scoreboard/widgets/circleLoader.dart';
 import 'package:game_scoreboard/widgets/errorDisplay.dart';
 import 'package:game_scoreboard/widgets/gameAvatar.dart';
@@ -13,7 +15,11 @@ import 'package:game_scoreboard/widgets/standingList.dart';
 Screen: Game
 */
 class GameScreen extends StatefulWidget {
-  GameScreen({Key key, this.gameId, this.gameName}) : super(key: key);
+  GameScreen({
+    Key key,
+    this.gameId,
+    this.gameName
+  }) : super(key: key);
 
   final int gameId;
   final String gameName;
@@ -24,62 +30,84 @@ class GameScreen extends StatefulWidget {
 
 class _GameScreenState extends State<GameScreen> {
   Future<Game> _game;
-  Future<List> _standings;
+
+  void _changeAvatar(Game game) {
+    print("inasdg");
+  }
+
+  void _goToGameEdit(Game game) {
+    print("in edit");
+  }
+
+  void _deleteGame(Game game) {
+    print("in delete");
+  }
 
   @override
   void initState() {
-    _game = Game().fetch(widget.gameId);
-    _standings = Game().standings(widget.gameId);
-
+    _game = Game.fetch(widget.gameId);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.gameName),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: ListView(
-          children: <Widget>[
-            FutureBuilder<Game>(
-                future: _game,
-                builder: (BuildContext context, AsyncSnapshot<Game> snapshot) {
-                  if (snapshot.hasData) {
-                    return _GameBody(game: snapshot.data);
-                  } else if (snapshot.hasError) {
-                    return ErrorDisplay(
-                      errorMessage: 'Could not load game data',
-                    );
-                  }
+    final AppBar appBar = AppBar(
+      title: Text(widget.gameName),
+    );
 
-                  return CircleLoader();
-                }
+    return FutureBuilder<Game>(
+      future: _game,
+      builder: (BuildContext context, AsyncSnapshot<Game> snapshot) {
+        if (snapshot.hasData) {
+          final Game game = snapshot.data;
+
+          return AppScaffold(
+            top: appBar,
+            bottom: Provider.of<CurrentPlayer>(context, listen: false).isAdmin() ? AppEditBar(
+              record: game,
+              avatarCallback: _changeAvatar,
+              editCallback: _goToGameEdit,
+              deleteCallback: _deleteGame,
+            ) : null,
+            center: ListView(
+              children: <Widget>[
+                _GameBody(game: game),
+                FutureBuilder(
+                  future: game.standings(),
+                  builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+                    if (snapshot.hasData) {
+                      final List<dynamic> standingsData = snapshot.data;
+
+                      return StandingList(
+                        standingsData: standingsData,
+                      );
+                    } else if (snapshot.hasError) {
+                      return ErrorDisplay(
+                        errorMessage: 'Could not load standings table',
+                      );
+                    }
+
+                    // By default, show a loading spinner.
+                    return CircleLoader();
+                  },
+                ),
+              ],
             ),
-            FutureBuilder(
-                future: _standings,
-                builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-                  if (snapshot.hasData) {
-                    final List<dynamic> standingsData = snapshot.data;
-
-                    return StandingList(
-                      standingsData: standingsData,
-                    );
-                  } else if (snapshot.hasError) {
-                    return ErrorDisplay(
-                      errorMessage: 'Could not load standings table',
-                    );
-                  }
-
-                  // By default, show a loading spinner.
-                  return CircleLoader();
-                }
+          );
+        } else if (snapshot.hasError) {
+          return AppScaffold(
+            top: appBar,
+            center: ErrorDisplay(
+              errorMessage: 'Could not load game data',
             ),
-          ],
-        ),
-      )
+          );
+        }
+
+        return AppScaffold(
+          top: appBar,
+          center: CircleLoader(),
+        );
+      },
     );
   }
 }
--- lib/screens/playerScreen.dart
+++ lib/screens/playerScreen.dart
@@ -1,8 +1,11 @@
 // flutter
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 // dependencies
 // app
+import 'package:game_scoreboard/models/appProviders/currentPlayer.dart';
 import 'package:game_scoreboard/models/player.dart';
+import 'package:game_scoreboard/widgets/appBlocks.dart';
 import 'package:game_scoreboard/widgets/circleLoader.dart';
 import 'package:game_scoreboard/widgets/errorDisplay.dart';
 import 'package:game_scoreboard/widgets/playerCard.dart';
@@ -25,42 +28,45 @@ class PlayerScreen extends StatefulWidget {
 
 class _PlayerScreenState extends State<PlayerScreen> {
   Future<Player> _player;
-  Future<List> _standings;
+
+  void _goToPlayerEdit(Player player) {
+    print("in edit");
+  }
+
+  void _deletePlayer(Player player) {
+    print("in delete");
+  }
 
   @override
   void initState() {
-    _player = Player().fetch(widget.playerId);
-    _standings = Player().standings(widget.playerId);
-
+    _player = Player.fetch(widget.playerId);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.playerNickname),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: ListView(
-            children: <Widget>[
-              FutureBuilder<Player>(
-                  future: _player,
-                  builder: (BuildContext context, AsyncSnapshot<Player> snapshot) {
-                    if (snapshot.hasData) {
-                      return PlayerCard(player: snapshot.data);
-                    } else if (snapshot.hasError) {
-                      return ErrorDisplay(
-                        errorMessage: 'Could not load player data',
-                      );
-                    }
+    final AppBar appBar = AppBar(
+      title: Text(widget.playerNickname),
+    );
 
-                    return CircleLoader();
-                  }
-              ),
-              FutureBuilder(
-                  future: _standings,
+    return FutureBuilder<Player>(
+      future: _player,
+      builder: (BuildContext context, AsyncSnapshot<Player> snapshot) {
+        if (snapshot.hasData) {
+          final Player player = snapshot.data;
+
+          return AppScaffold(
+            top: appBar,
+            bottom: Provider.of<CurrentPlayer>(context, listen: false).isAdmin() ? AppEditBar(
+              record: player,
+              editCallback: _goToPlayerEdit,
+              deleteCallback: _deletePlayer,
+            ) : null,
+            center: ListView(
+              children: <Widget>[
+                PlayerCard(player: player),
+                FutureBuilder(
+                  future: player.standings(),
                   builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
                     if (snapshot.hasData) {
                       final List<dynamic> standingsData = snapshot.data;
@@ -76,11 +82,25 @@ class _PlayerScreenState extends State<PlayerScreen> {
 
                     // By default, show a loading spinner.
                     return CircleLoader();
-                  }
-              ),
-            ],
-          ),
-        )
+                  },
+                ),
+              ],
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return AppScaffold(
+            top: appBar,
+            center: ErrorDisplay(
+              errorMessage: 'Could not load game data',
+            ),
+          );
+        }
+
+        return AppScaffold(
+          top: appBar,
+          center: CircleLoader(),
+        );
+      },
     );
   }
 }
