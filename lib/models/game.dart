dbcdea57d33ae5dfc99d9f941f7a7d172899623a
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-09-20 10:39:08 -0400

    added attribute for avatar url to display game images

--- lib/models/game.dart
+++ lib/models/game.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 // dependencies
 import 'package:json_annotation/json_annotation.dart';
 // app
+import 'package:game_scoreboard/models/jsonModel.dart';
 import 'package:game_scoreboard/services/apiServices.dart';
 
 // serializer
@@ -13,8 +14,8 @@ part 'game.g.dart';
 model: Game
 */
 @JsonSerializable()
-class Game {
-  @JsonKey(fromJson: _stringToInt, toJson: _stringFromInt)
+class Game with JsonModel {
+  @JsonKey(fromJson: JsonModel.stringToInt, toJson: JsonModel.stringFromInt)
   final int id;
   final String name;
   final String description;
@@ -26,29 +27,27 @@ class Game {
   final int minPlayTime;
   @JsonKey(name: 'max_play_time')
   final int maxPlayTime;
+  @JsonKey(name: 'avatar_url')
+  final String avatarUrl;
 
   Game({
     this.id, this.name, this.description,
     this.minPlayers, this.maxPlayers,
     this.minPlayTime, this.maxPlayTime,
+    this.avatarUrl,
   });
 
   static String _apiPath = 'v1/games';
 
   Future<Game> fetch(int id) async {
     final String url = '$_apiPath/$id';
-    Game game;
-
     final response = await api.get(url);
+    final Map<String, dynamic> responseData = parseResponseString(
+      responseString: response.body,
+      responseCode: response.statusCode,
+    );
 
-    final responseJson = json.decode(response.body);
-    final responseData = responseJson['data'];
-
-    Map<String, dynamic> gameData = {};
-    gameData.addAll({'id': responseData['id']});
-    gameData.addAll(responseData['attributes']);
-
-    game = Game.fromJson(gameData);
+    final Game game = Game.fromJson(parseResponseDataToRecordData(responseData: responseData));
     return game;
   }
 
@@ -64,7 +63,4 @@ class Game {
 
   factory Game.fromJson(Map<String, dynamic> json) => _$GameFromJson(json);
   Map<String, dynamic> toJson() => _$GameToJson(this);
-
-  static int _stringToInt(String number) => number == null ? null : int.parse(number);
-  static String _stringFromInt(int number) => number?.toString();
 }
--- lib/models/game.g.dart
+++ lib/models/game.g.dart
@@ -8,22 +8,24 @@ part of 'game.dart';
 
 Game _$GameFromJson(Map<String, dynamic> json) {
   return Game(
-    id: Game._stringToInt(json['id'] as String),
+    id: JsonModel.stringToInt(json['id'] as String),
     name: json['name'] as String,
     description: json['description'] as String,
     minPlayers: json['min_players'] as int,
     maxPlayers: json['max_players'] as int,
     minPlayTime: json['min_play_time'] as int,
     maxPlayTime: json['max_play_time'] as int,
+    avatarUrl: json['avatar_url'] as String,
   );
 }
 
 Map<String, dynamic> _$GameToJson(Game instance) => <String, dynamic>{
-      'id': Game._stringFromInt(instance.id),
+      'id': JsonModel.stringFromInt(instance.id),
       'name': instance.name,
       'description': instance.description,
       'min_players': instance.minPlayers,
       'max_players': instance.maxPlayers,
       'min_play_time': instance.minPlayTime,
       'max_play_time': instance.maxPlayTime,
+      'avatar_url': instance.avatarUrl,
     };
