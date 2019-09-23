9a1dda68ad2c5c88bd5f4f87033b7f416548185e
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-09-23 08:35:27 -0400

    ios deployment

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/juanmaberrocal/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/juanmaberrocal/game_scoreboard_ui"
+export "FLUTTER_TARGET=/Users/juanmaberrocal/game_scoreboard_ui/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/juanmaberrocal/development/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0"
